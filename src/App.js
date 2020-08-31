f8e4ec32e9d89a6087782eaf2dd7d9377137e28d
ZerrinBostan <46298014+ZerrinBostan@users.noreply.github.com>
2020-08-31 11:14:34 +0300

    App.js page was created.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header, Card } from './components';
+import './styles/index.scss';
+
+function App() {
+  return (
+    <div className="App app-wrapper">
+      <Card>
+        <Header />
+      </Card>
+    </div>
+  );
+}
+
+export default App;
